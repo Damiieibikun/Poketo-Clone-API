450148a83422c08779f1438eb402a307d0f99386
Damilola Ibikunle <damiieibikun@gmail.com>
2024-07-23 22:47:50 +0100

    got all reviews and ratings

--- user.js
+++ user.js
@@ -1,48 +1,133 @@
-$(document).ready(()=>{
-    const endPoint = "http://ecommerce.reworkstaging.name.ng/v2";
-let userDetails = JSON.parse(localStorage.getItem('LoggedUser'))
+$(document).ready(() => {
+  const endPoint = "http://ecommerce.reworkstaging.name.ng/v2";
+  let userDetails = JSON.parse(localStorage.getItem("LoggedUser"));
 
-$('#d-userBioDetails').html(`
+  $("#d-userBioDetails").html(`
     <span>${userDetails.first_name}</span> <span>${userDetails.last_name}</span>
     <p>${userDetails.email}</p>
     <p>${userDetails.phone}</p>
-    `)
+    `);
 
-
-$.ajax({
-url:`${endPoint}/users/likes?user_id=${userDetails.id}`,
-method: 'GET',
-success: function(res){
-    if(res.length > 0){
-        $('#no-likes').addClass('d-display-none')
+  $.ajax({
+    url: `${endPoint}/users/likes?user_id=${userDetails.id}`,
+    method: "GET",
+    success: function (res) {
+      if (res.length > 0) {
+        $("#no-likes").addClass("d-display-none");
         res.forEach((element, i) => {
-            let productInfo = element.product_id
-                     
-            $('#user-order-likes').append(`<div class="d-flex d-align-center d-gap-20">
-                                <span>${i+1}</span>
-                                <div class="d-likedProductIMG" style=" background-image: url(${productInfo.images[0]});"></div>
-                                <p class="d-likedProductName">${productInfo.title}</p>
+          let productInfo = element.product_id;
+
+          $("#user-order-likes")
+            .append(`<div class="d-flex d-align-center d-gap-20">
+                                <!-- <span>${i + 1}</span> -->
+                                <div class="d-likedProductIMG" style=" background-image: url(${
+                                  productInfo.images[0]
+                                });"></div>
+                                <p class="d-likedProductName">${
+                                  productInfo.title
+                                }</p>
+                               
                                 <p class="d-numofLikes"></p>
-                             </div>`)
+                             </div>`);
         });
-    }
+      } else {
+        $("#no-likes").removeClass("d-display-none");
+      }
+    },
+    error: function (err) {
+      console.log(err);
+    },
+  });
 
-    else{
-        $('#no-likes').removeClass('d-display-none')
-    }
-  
-},
-error: function(err){
-    console.log(err)
-}
-})
+  // customer's reviews
+  $.ajax({
+    url: `${endPoint}/users/reviews?user_id=${userDetails.id}`,
+    method: "GET",
+    success: function (res) {
+      if (res.length > 0) {
+        $("#no-reviews").addClass("d-display-none");
+        console.log(res);
+        res.forEach((review) => {
+          $.ajax({
+            url: `${endPoint}/ratings?product_id=${review.product_id._id}`,
+            method: "GET",
+            success: function (rating) {
+              rating.forEach((item) => {
+                if (item.user.id === userDetails.id) {
+                  let productReview =
+                    $(`<div class="d-grid d-align-center d-gap-20 d-customer-reviewItems-grid">
+                    <div>
+                     <p class="d-likedProductName">${review.product_id.title}</p>
+                    <div class="d-likedProductIMG" style=" background-image: url(${review.product_id.images[0]});"></div>
+                                   
+                    </div>
+                                    
+                                    <div class="d-flex-col">
+                                     <b>Review:</b>
+                                    <p class='customerReview'>${review.text}</p>
 
+                                    <div id='d-item-review-stars'>
+                                                <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                                    <path fill-rule="evenodd" clip-rule="evenodd" d="M7.99998 13.0852L3.28889 15.4762C3.18255 15.5302 3.05189 15.4891 2.99706 15.3844C2.97577 15.3438 2.96832 15.2975 2.97581 15.2523L3.83017 10.103L0.064192 6.43136C-0.0208179 6.34848 -0.0214783 6.21346 0.062717 6.12978C0.0954128 6.09728 0.137856 6.07599 0.183781 6.06906L5.4229 5.27766L7.80648 0.617401C7.86029 0.512205 7.99054 0.469862 8.09741 0.522826C8.13891 0.543393 8.17259 0.57655 8.19349 0.617401L10.5771 5.27766L15.8162 6.06906C15.9345 6.08692 16.0156 6.19578 15.9975 6.31219C15.9904 6.3574 15.9688 6.39918 15.9358 6.43136L12.1698 10.103L13.0242 15.2523C13.0434 15.3686 12.9634 15.4782 12.8453 15.4972C12.7994 15.5045 12.7524 15.4972 12.7111 15.4762L7.99998 13.0852Z" fill="#dedede"></path>
+                                                </svg>
+                                                <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                                    <path fill-rule="evenodd" clip-rule="evenodd" d="M7.99998 13.0852L3.28889 15.4762C3.18255 15.5302 3.05189 15.4891 2.99706 15.3844C2.97577 15.3438 2.96832 15.2975 2.97581 15.2523L3.83017 10.103L0.064192 6.43136C-0.0208179 6.34848 -0.0214783 6.21346 0.062717 6.12978C0.0954128 6.09728 0.137856 6.07599 0.183781 6.06906L5.4229 5.27766L7.80648 0.617401C7.86029 0.512205 7.99054 0.469862 8.09741 0.522826C8.13891 0.543393 8.17259 0.57655 8.19349 0.617401L10.5771 5.27766L15.8162 6.06906C15.9345 6.08692 16.0156 6.19578 15.9975 6.31219C15.9904 6.3574 15.9688 6.39918 15.9358 6.43136L12.1698 10.103L13.0242 15.2523C13.0434 15.3686 12.9634 15.4782 12.8453 15.4972C12.7994 15.5045 12.7524 15.4972 12.7111 15.4762L7.99998 13.0852Z" fill="#dedede"></path>
+                                                </svg>
+                                                <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                                    <path fill-rule="evenodd" clip-rule="evenodd" d="M7.99998 13.0852L3.28889 15.4762C3.18255 15.5302 3.05189 15.4891 2.99706 15.3844C2.97577 15.3438 2.96832 15.2975 2.97581 15.2523L3.83017 10.103L0.064192 6.43136C-0.0208179 6.34848 -0.0214783 6.21346 0.062717 6.12978C0.0954128 6.09728 0.137856 6.07599 0.183781 6.06906L5.4229 5.27766L7.80648 0.617401C7.86029 0.512205 7.99054 0.469862 8.09741 0.522826C8.13891 0.543393 8.17259 0.57655 8.19349 0.617401L10.5771 5.27766L15.8162 6.06906C15.9345 6.08692 16.0156 6.19578 15.9975 6.31219C15.9904 6.3574 15.9688 6.39918 15.9358 6.43136L12.1698 10.103L13.0242 15.2523C13.0434 15.3686 12.9634 15.4782 12.8453 15.4972C12.7994 15.5045 12.7524 15.4972 12.7111 15.4762L7.99998 13.0852Z" fill="#dedede"></path>
+                                                </svg>
+                                                <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                                    <path fill-rule="evenodd" clip-rule="evenodd" d="M7.99998 13.0852L3.28889 15.4762C3.18255 15.5302 3.05189 15.4891 2.99706 15.3844C2.97577 15.3438 2.96832 15.2975 2.97581 15.2523L3.83017 10.103L0.064192 6.43136C-0.0208179 6.34848 -0.0214783 6.21346 0.062717 6.12978C0.0954128 6.09728 0.137856 6.07599 0.183781 6.06906L5.4229 5.27766L7.80648 0.617401C7.86029 0.512205 7.99054 0.469862 8.09741 0.522826C8.13891 0.543393 8.17259 0.57655 8.19349 0.617401L10.5771 5.27766L15.8162 6.06906C15.9345 6.08692 16.0156 6.19578 15.9975 6.31219C15.9904 6.3574 15.9688 6.39918 15.9358 6.43136L12.1698 10.103L13.0242 15.2523C13.0434 15.3686 12.9634 15.4782 12.8453 15.4972C12.7994 15.5045 12.7524 15.4972 12.7111 15.4762L7.99998 13.0852Z" fill="#dedede"></path>
+                                                </svg>
+                                                <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                                    <path fill-rule="evenodd" clip-rule="evenodd" d="M7.99998 13.0852L3.28889 15.4762C3.18255 15.5302 3.05189 15.4891 2.99706 15.3844C2.97577 15.3438 2.96832 15.2975 2.97581 15.2523L3.83017 10.103L0.064192 6.43136C-0.0208179 6.34848 -0.0214783 6.21346 0.062717 6.12978C0.0954128 6.09728 0.137856 6.07599 0.183781 6.06906L5.4229 5.27766L7.80648 0.617401C7.86029 0.512205 7.99054 0.469862 8.09741 0.522826C8.13891 0.543393 8.17259 0.57655 8.19349 0.617401L10.5771 5.27766L15.8162 6.06906C15.9345 6.08692 16.0156 6.19578 15.9975 6.31219C15.9904 6.3574 15.9688 6.39918 15.9358 6.43136L12.1698 10.103L13.0242 15.2523C13.0434 15.3686 12.9634 15.4782 12.8453 15.4972C12.7994 15.5045 12.7524 15.4972 12.7111 15.4762L7.99998 13.0852Z" fill="#dedede"></path>
+                                                </svg>
+                                            </div>
+                                    </div>
 
+                                    <div class="d-flex d-gap-10">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pen-fill" viewBox="0 0 16 16">
+                        <path d="m13.498.795.149-.149a1.207 1.207 0 1 1 1.707 1.708l-.149.148a1.5 1.5 0 0 1-.059 2.059L4.854 14.854a.5.5 0 0 1-.233.131l-4 1a.5.5 0 0 1-.606-.606l1-4a.5.5 0 0 1 .131-.232l9.642-9.642a.5.5 0 0 0-.642.056L6.854 4.854a.5.5 0 1 1-.708-.708L9.44.854A1.5 1.5 0 0 1 11.5.796a1.5 1.5 0 0 1 1.998-.001"/>
+                      </svg>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash2-fill" viewBox="0 0 16 16">
+                        <path d="M2.037 3.225A.7.7 0 0 1 2 3c0-1.105 2.686-2 6-2s6 .895 6 2a.7.7 0 0 1-.037.225l-1.684 10.104A2 2 0 0 1 10.305 15H5.694a2 2 0 0 1-1.973-1.671zm9.89-.69C10.966 2.214 9.578 2 8 2c-1.58 0-2.968.215-3.926.534-.477.16-.795.327-.975.466.18.14.498.307.975.466C5.032 3.786 6.42 4 8 4s2.967-.215 3.926-.534c.477-.16.795-.327.975-.466-.18-.14-.498-.307-.975-.466z"/>
+                      </svg>
+                                    </div>
+                                 </div>`);
 
+                  // Fill the stars based on the rating
+                  let productRating = productReview
+                    .find("#d-item-review-stars")
+                    .find("svg");
+                  if (item.value > 0) {
+                    productRating.each((i, svg) => {
+                      $(svg).find("path").css("fill", "#ef4043");
+                      if (i === Math.round(item.value) - 1) {
+                        return false;
+                      }
+                    });
+                  }
+                  $("#user-order-reviews").append(productReview);
+                }
+              });
+            },
+            error: function (err) {
+              console.log(err);
+            },
+          });
+        });
+      } else {
+        $("#no-reviews").removeClass("d-display-none");
+      }
+    },
+    error: function (err) {
+      console.log(err);
+    },
+  });
 
-    //remove logged users details
-    $('#d-userslogout-link').click(function(){
-        localStorage.removeItem('LoggedUser')
-        localStorage.removeItem('Selected-Product')
-    })
-})
+  //remove logged users details
+  $("#d-userslogout-link").click(function () {
+    localStorage.removeItem("LoggedUser");
+    localStorage.removeItem("Selected-Product");
+  });
+});
