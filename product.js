d374bce534b6cb61a6515d061d48fba76bd75da9
Damilola Ibikunle <damiieibikun@gmail.com>
2024-07-27 17:43:50 +0100

    completed functionality for discount

--- product.js
+++ product.js
@@ -19,6 +19,8 @@ $(document).ready(() => {
           method: "GET",
           success: function (data) {
             productDiscount = data.discount;
+            let allVariations =
+              JSON.parse(localStorage.getItem("ProductV")) || [];
             if (data.variations.length !== 0) {
               data.variations[0].content.forEach((content) => {
                 if (content.display[0].type === "image") {
@@ -27,8 +29,7 @@ $(document).ready(() => {
                   productColor.push(content.display[0].value);
                 }
               });
-              let allVariations =
-                JSON.parse(localStorage.getItem("ProductV")) || [];
+
               let productVariationsInfo = {
                 product_id: res.id,
                 availableImage: productImages,
@@ -36,9 +37,14 @@ $(document).ready(() => {
                 productDiscount: productDiscount,
               };
               allVariations.push(productVariationsInfo);
-
-              localStorage.setItem("ProductV", JSON.stringify(allVariations));
+            } else {
+              let productVariationsInfo = {
+                product_id: res.id,
+                productDiscount: productDiscount,
+              };
+              allVariations.push(productVariationsInfo);
             }
+            localStorage.setItem("ProductV", JSON.stringify(allVariations));
           },
           error: function (err) {
             console.log(err);
@@ -128,7 +134,7 @@ $(document).ready(() => {
                                <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M7.99998 13.0852L3.28889 15.4762C3.18255 15.5302 3.05189 15.4891 2.99706 15.3844C2.97577 15.3438 2.96832 15.2975 2.97581 15.2523L3.83017 10.103L0.064192 6.43136C-0.0208179 6.34848 -0.0214783 6.21346 0.062717 6.12978C0.0954128 6.09728 0.137856 6.07599 0.183781 6.06906L5.4229 5.27766L7.80648 0.617401C7.86029 0.512205 7.99054 0.469862 8.09741 0.522826C8.13891 0.543393 8.17259 0.57655 8.19349 0.617401L10.5771 5.27766L15.8162 6.06906C15.9345 6.08692 16.0156 6.19578 15.9975 6.31219C15.9904 6.3574 15.9688 6.39918 15.9358 6.43136L12.1698 10.103L13.0242 15.2523C13.0434 15.3686 12.9634 15.4782 12.8453 15.4972C12.7994 15.5045 12.7524 15.4972 12.7111 15.4762L7.99998 13.0852Z" fill="#f6ede6"></path></svg>
                                <span class="d-slider-product-rating">${avgRating} <span class='d-quantity-avail'>(${product.quantity})</span></span>
                            </div>
-                           <div>
+                           <div class="d-flex d-gap-10">
                             <span class="d-slider-product-price" style="text-decoration: ${discountStyle}; color: ${colorStyle}">$${product.price}</span> <span class="d-discountedPriceValue" style = "display:${showDiscount}"></span>
                             </div>
                        </div>
@@ -168,21 +174,27 @@ $(document).ready(() => {
                         (productItems.productDiscount / 100) * product.price
                     )}`
                   );
-                if (productItems.availableImage.length > 0) {
-                  productItems.availableImage.forEach((img, i) => {
-                    productItems_shop.find(".d-product-colors")
-                      .append(`<div class="d-color-selection-outer">
-                         <div class="d-color-selection" data-id=${i}" style="background-image: url(${img}); background-color: transparent"></div>
-                     </div>`);
-                  });
-                }
-                if (productItems.availableColors.length > 0) {
-                  productItems.availableColors.forEach((color, i) => {
-                    productItems_shop.find(".d-product-colors")
-                      .append(`<div class="d-color-selection-outer">
-                         <div class="d-color-selection" data-id=${i}" style="background-image: url(''); background-color: ${color}"></div>
-                     </div>`);
-                  });
+
+                if (
+                  "avaliableImage" in productItems ||
+                  "availableColors" in productItems
+                ) {
+                  if (productItems.availableImage.length > 0) {
+                    productItems.availableImage.forEach((img, i) => {
+                      productItems_shop.find(".d-product-colors")
+                        .append(`<div class="d-color-selection-outer">
+                             <div class="d-color-selection" data-id=${i}" style="background-image: url(${img}); background-color: transparent"></div>
+                         </div>`);
+                    });
+                  }
+                  if (productItems.availableColors.length > 0) {
+                    productItems.availableColors.forEach((color, i) => {
+                      productItems_shop.find(".d-product-colors")
+                        .append(`<div class="d-color-selection-outer">
+                             <div class="d-color-selection" data-id=${i}" style="background-image: url(''); background-color: ${color}"></div>
+                         </div>`);
+                    });
+                  }
                 }
               }
             });
@@ -274,7 +286,7 @@ $(document).ready(() => {
               tag: "New!",
               tagColor: "#73ccf3",
             };
-          } else if (avgRating > 3.5 && res.like > 0) {
+          } else if (res.review > 1 && avgRating > 4 && res.like > 0) {
             var selectedProductTag = {
               tag: "Best Seller",
               tagColor: "#ffc845",
@@ -371,13 +383,15 @@ $(document).ready(() => {
             });
           }
 
-          $('#j-selectedProduct-info').find(".d-product-colors").each((index, i) => {
-            if ($(i).children()[0]) {
-              $(i).children()[0].classList.add("d-selected-color");
-              console.log($(i).children()[0].id)
-              $('#d-variantSelection').text($(i).children()[0].id)
-            }
-          });
+          $("#j-selectedProduct-info")
+            .find(".d-product-colors")
+            .each((index, i) => {
+              if ($(i).children()[0]) {
+                $(i).children()[0].classList.add("d-selected-color");
+                console.log($(i).children()[0].id);
+                $("#d-variantSelection").text($(i).children()[0].id);
+              }
+            });
 
           // get all products liked by a user and compare
           let isLiked = false;
